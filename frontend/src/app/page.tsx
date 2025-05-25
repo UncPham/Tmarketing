610b3f1a6e3cfb2b8f887481e58000a81d62d3ea
ChienPhamUET <22022634@vnu.edu.vn>
2025-05-25 13:56:36 +0700

    image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,13 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative w-full h-screen">
-      <Image
-        src="/background.jpg"
-        alt="Background"
-        fill
-        className="object-cover"
-        priority
-      />
+      <img 
+      src="/background.jpg" 
+      alt="Background" 
+      className="object-cover" 
+      style={{ position: 'absolute', height: '100%', width: '100%', left: 0, top: 0, right: 0, bottom: 0, color: 'transparent' }} />
     </div>
   );
 }
